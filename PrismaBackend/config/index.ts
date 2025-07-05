cf471866c0ea8b477360b379e80813ee6e3a6fb8
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-07-06 00:35:50 +0530

    database tables intialized successfully

---
+++ PrismaBackend/config/index.ts
@@ -0,0 +1,9 @@
+ // src/config/index.ts
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const PORT = process.env.PORT || 3000;
+export const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key'; // Fallback for development, strongly recommend env var
+export const GRACE_PERIOD_MINUTES = 15;
+export const DATABASE_URL = process.env.DATABASE_URL || "postgres://612a2dd0a487491a9b401ca7ed535fbb5211055040111e2935a59fc4f9bc9470:sk_RHPsCwIffMVj_2Z6Yfwha@db.prisma.io:5432/?sslmode=require";
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
---
+++ PrismaBackend/db/dbIntiliaze.ts
@@ -0,0 +1,60 @@
+import {Pool} from 'pg'
+import {DATABASE_URL} from  '../config/index'
+
+
+export const pool=new Pool({
+    connectionString:DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, 
+})
+
+
+export async function initilizeDatabase():Promise<void>{
+    let client ;
+    try{
+        client=await pool.connect() 
+        await client.query(`
+                CREATE TABLE IF NOT EXISTS  users (
+                id SERIAL PRIMARY KEY ,
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP ,
+                );
+
+                CREATE TABLE IF NOT EXISTS  comments (
+                    id SERIAL PRIMARY KEY ,
+                    user_id INTEGER NOT NULL REFRENCES users(id) ON DELETE CASCADE ,
+                    parent_id INTEGER REFERENCES comments(id) ON DELETE CASCADE ,
+                    content  TEXT NOT NULL ,
+                    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP ,
+                    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP ,
+                    deleted_at TIMESTAMP WITH TIME ZONE ,
+                    is_deleted BOOLEAN DEFAULT FALSE 
+                ) ;
+
+                CREATE TABLE IF NOT EXISTS  notifications  (
+                    id SERIAL PRIMARY KEY ,
+                    user_id INTEGER NOT NULL REFERENCES user(id) ON DELETE CASCADE ,
+                    comment_id INTEGER NOT NULL REFERENCES  comments(id) on DELETE CASCADE ,
+                    reply_id  INTEGER NOT NULL REFRENCESE comments(id) ON DELETE CASCADE,
+                    message TEXT NOT NULL ,
+                    is_read BOOLEAN DEFAULT FALSE ,
+                    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
+                ) ;
+
+               CREATE INDEX IF NOT EXISTS idx_comments_user_id ON  comments(user_id) ;
+               CREATE INDEX IF NOT EXISTS idx_comments_parent_id ON comments(parent_id) ;
+               CREATE INDEX IF NOT EXISTS idx_notifications_user_id ON notifications(user_id) ;
+               CREATE INDEX IF NOT EXISTS idx_notifications_comment_id ON notifications(comment_id) 
+
+            `)
+
+            console.log('database tabled intialized successfully') 
+    }catch(err){
+           console.error('error intializing in database',err) 
+    }
+    finally{
+        if(client){
+            client.release() 
+        }
+    }
+}
